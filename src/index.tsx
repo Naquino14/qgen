779e226afa182079bef286f76b86228435ead39e
Nathaniel Aquino <naquino14@outlook.com>
2022-10-09 22:19:56 -0400

    Basic QR function generator

--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,72 @@
 import React from 'react'
+import { FinderPattern, AlignmentPattern } from './patterns'
 
 type Props = {
-  width: number
-  height: number
+  width?: number | null
+  height?: number | null
   squareLength: number
+  payload: string
   style?: React.CSSProperties
-  squares: qsquare[][]
-  falseColor?: string
-  trueColor?: string
+  squares: boolean[][]
+  falseColor?: string | null
+  trueColor?: string | null
 }
 
-export const index: React.FC = () => {
+export const i: React.FC = () => {
   return <div>index</div>
 }
 
-export type qsquare = {
-  a: boolean
+export const StampCode = (base: boolean[][], stamp: boolean[][], x: number = 0, y: number = 0) => {
+  let result: boolean[][] = base
+  for (let i = 0; i < stamp.length; i++) {
+    for (let j = 0; j < stamp[i].length; j++) {
+      result[i + y][j + x] = stamp[i][j]
+    }
+  }
+  return result
 }
 
 export const QrRenderer: React.FC<Props> = ({
-  width,
-  height,
+  width = null,
+  height = null,
   squareLength,
+  payload,
   style,
-  squares,
   falseColor = null,
   trueColor = null,
 }) => {
+  let qr: boolean[][] = []
+
+  for (let i = 0; i < 57; i++) {
+    qr[i] = new Array(57).fill(false)
+  }
+
+  // Stamp finder patterns
+  qr = StampCode(qr, FinderPattern, 0, 0)
+  qr = StampCode(qr, FinderPattern, 50, 0)
+  qr = StampCode(qr, FinderPattern, 0, 50)
+
+  // Stamp alignment patterns
+  qr = StampCode(qr, AlignmentPattern, 26, 4)
+  qr = StampCode(qr, AlignmentPattern, 4, 26)
+  qr = StampCode(qr, AlignmentPattern, 26, 26)
+  qr = StampCode(qr, AlignmentPattern, 48, 26)
+  qr = StampCode(qr, AlignmentPattern, 26, 48)
+  qr = StampCode(qr, AlignmentPattern, 48, 48)
+
+  // Stamp timing pattern
+  for (let i = 7; i < 50; i++) {
+    const o = i % 2 === 0
+    qr[i][6] = o
+    qr[6][i] = o
+  }
+
+
+
   return (
     <div>
-      <svg width={width} height={height} style={style}>
-        {squares.map((row, x) =>
+      <svg width={width ?? 57 * squareLength} height={height ?? 57 * squareLength} style={style}>
+        {qr.map((row, x) =>
           row.map((square, y) => (
             <rect
               key={`${x}-${y}`}
@@ -38,7 +74,7 @@ export const QrRenderer: React.FC<Props> = ({
               y={x * squareLength}
               width={10}
               height={10}
-              fill={square.a ? trueColor ?? 'black' : falseColor ?? 'white'}
+              fill={square ? trueColor ?? 'black' : falseColor ?? 'white'}
             />
           )),
         )}
---
+++ src/patterns.ts
@@ -0,0 +1,31 @@
+export const FinderPattern: boolean[][] = [
+    [
+        true, true, true, true, true, true, true
+    ],
+    [
+        true, false, false, false, false, false, true
+    ],
+    [
+        true, false, true, true, true, false, true
+    ],
+    [
+        true, false, true, true, true, false, true
+    ],
+    [
+        true, false, true, true, true, false, true
+    ],
+    [
+        true, false, false, false, false, false, true
+    ],
+    [
+        true, true, true, true, true, true, true
+    ]
+]
+
+export const AlignmentPattern = [
+    [true, true, true, true, true],
+    [true, false, false, false, true],
+    [true, false, true, false, true],
+    [true, false, false, false, true],
+    [true, true, true, true, true]
+]
