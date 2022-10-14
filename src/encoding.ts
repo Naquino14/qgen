b28d51abd197bfddacb9af39eefaede79fececba
Nathaniel Aquino <naquino14@outlook.com>
2022-10-14 12:00:10 -0400

    WIP ECI Encoding

--- src/encoding.ts
+++ src/encoding.ts
@@ -1,6 +1,6 @@
 export const GenV10ECIheader = (payload: string) => {
-    const payloadLength = payload.length
-    const alphaNumericECI = [false, false, true, false]
-    const header: boolean[] = []
-    header.push(...alphaNumericECI)
+  const payloadLength = payload.length
+  const alphaNumericECI = [false, false, true, false]
+  const header: boolean[] = []
+  header.push(...alphaNumericECI)
 }
--- src/patterns.ts
+++ src/patterns.ts
@@ -1,17 +1,17 @@
 export const FinderPattern: boolean[][] = [
-    [true, true, true, true, true, true, true],
-    [true, false, false, false, false, false, true],
-    [true, false, true, true, true, false, true],
-    [true, false, true, true, true, false, true],
-    [true, false, true, true, true, false, true],
-    [true, false, false, false, false, false, true],
-    [true, true, true, true, true, true, true]
+  [true, true, true, true, true, true, true],
+  [true, false, false, false, false, false, true],
+  [true, false, true, true, true, false, true],
+  [true, false, true, true, true, false, true],
+  [true, false, true, true, true, false, true],
+  [true, false, false, false, false, false, true],
+  [true, true, true, true, true, true, true],
 ]
 
 export const AlignmentPattern = [
-    [true, true, true, true, true],
-    [true, false, false, false, true],
-    [true, false, true, false, true],
-    [true, false, false, false, true],
-    [true, true, true, true, true]
+  [true, true, true, true, true],
+  [true, false, false, false, true],
+  [true, false, true, false, true],
+  [true, false, false, false, true],
+  [true, true, true, true, true],
 ]
