fc36faecf88e7a9dcad749a37a19367f2b7b6fb0
Nathaniel Aquino <naquino14@outlook.com>
2022-10-15 18:12:51 -0400

    1.0.10

--- src/encoding.ts
+++ src/encoding.ts
@@ -32,8 +32,7 @@ export const GenPayload = (payload: string, headerFunction: (payload: string) =>
 
   // step 1: convert payload to char codes
   const charCodes: number[] = []
-  for (let i = 0; i < payloadLength; i++)
-    charCodes.push(AlphanumericTable.indexOf(payload[i]))
+  for (let i = 0; i < payloadLength; i++) charCodes.push(AlphanumericTable.indexOf(payload[i]))
 
   // step 2: group char codes into groups of 2
   const charCodeGroups: number[][] = []
@@ -46,10 +45,8 @@ export const GenPayload = (payload: string, headerFunction: (payload: string) =>
 
   // step 3: convert char codes to bits (11 bits per group) and push to payload bits
   for (const group of charCodeGroups) {
-    if (group.length === 1)
-      payloadBits.push(...ConvertToBits(group[0], 6))
-    else
-      payloadBits.push(...ConvertToBits(group[0] * 45 + group[1], 11))
+    if (group.length === 1) payloadBits.push(...ConvertToBits(group[0], 6))
+    else payloadBits.push(...ConvertToBits(group[0] * 45 + group[1], 11))
   }
 
   // step 4: add terminator
@@ -60,15 +57,54 @@ export const GenPayload = (payload: string, headerFunction: (payload: string) =>
 
 const ConvertToBits = (value: number, length: number) => {
   const bits: boolean[] = []
-  for (let i = 0; i < length; i++)
-    bits.push((value >> i) % 2 === 1)
+  for (let i = 0; i < length; i++) bits.push((value >> i) % 2 === 1)
   return bits
 }
 
 const AlphanumericTable = [
-  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-  'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-  'U', 'V', 'W', 'X', 'Y', 'Z', ' ', '$', '%', '*',
-  '+', '-', '.', '/', ':'
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+  ' ',
+  '$',
+  '%',
+  '*',
+  '+',
+  '-',
+  '.',
+  '/',
+  ':',
 ]
