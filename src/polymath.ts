ac8d716d15df402dfe12bbc8b23a0ff3f8264de3
Nathaniel Aquino <naquino14@outlook.com>
2023-08-30 22:33:01 -0400

    Aded GF(256) log/antilog arrays

--- src/polymath.ts
+++ src/polymath.ts
@@ -39,3 +39,39 @@ export const PolynomialToBitString = (polynomial: Term[] | string): boolean[] =>
 
   return bitString
 }
+
+export const Mod285 = (exp: number): number => {
+  // base case: the exponent is 8
+  if (exp == 8)
+    return 29 // return 2^8 mod 285
+  if (exp < 8) // or its less than 8
+    return Math.pow(2, exp) // return itself as a power of 2
+  // recursive case: n is greater than 256
+  let r = 2 * Mod285(exp - 1)
+  if (r >= 256)
+    r ^= 285
+  return r
+}
+
+// export const NumToGF256 = (n: number): Term[] => {
+//   // if the number n is too large to be represented in GF(256), then we need to mod it
+//   if (n >= 256) {
+//     const r = Mod285(n)
+//     return NumToGF256(r)
+//   }
+//   const terms: Term[] = []
+//   let p = 0
+//   while (Math.pow(2, p) < n && p < 8) {
+//     p++
+//   }
+//   return terms
+// }
+
+export const GF256AlphaToInt: number[] = []
+for (let i = 0; i < 256; i++)
+  GF256AlphaToInt[i] = Mod285(i)
+
+export const GF256IntToAlpha: number[] = []
+GF256IntToAlpha[0] = -1
+for (let i = 1; i < 256; i++)
+  GF256IntToAlpha[i] = GF256AlphaToInt.indexOf(i)
