3e7f68f5c72575c050f994666139ac66b7ee9444
Nathaniel Aquino <naquino14@outlook.com>
2023-09-13 20:30:08 -0400

    Cleaned up polymath

--- src/polymath.ts
+++ src/polymath.ts
@@ -53,20 +53,6 @@ export const Mod285 = (exp: number): number => {
   return r
 }
 
-// export const NumToGF256 = (n: number): Term[] => {
-//   // if the number n is too large to be represented in GF(256), then we need to mod it
-//   if (n >= 256) {
-//     const r = Mod285(n)
-//     return NumToGF256(r)
-//   }
-//   const terms: Term[] = []
-//   let p = 0
-//   while (Math.pow(2, p) < n && p < 8) {
-//     p++
-//   }
-//   return terms
-// }
-
 export const GF256AlphaToInt: number[] = []
 for (let i = 0; i < 256; i++)
   GF256AlphaToInt[i] = Mod285(i)
@@ -92,26 +78,23 @@ export const GenGeneratorPoly = (degree: number): number[] => {
   if (degree == 0)
     return []
   // base case: degree is 1, return (x - a^0) [0, 1, 0, 0]
-  if (degree == 1) {
+  if (degree == 1)
     return [0, 1, 0, 0]
-  } else { // otherwise
+  else { // otherwise
     // the current term group is (x - a^dgree) * GenGeneratorPoly(degree - 1)
     const ctg = [0, 1, degree - 1, 0] // current term group
     const ptg = GenGeneratorPoly(degree - 1) // previous term group
     // foil the current term group with the previous term group
     const rtg: number[] = [] // resulting term group
-    for (let c = 0; c < ctg.length; c += 2) {
+    for (let c = 0; c < ctg.length; c += 2)
       for (let p = 0; p < ptg.length; p += 2) {
         // make sure that the alpha exponent is less than 255
         let raExp = ctg[c] + ptg[p]
         if (raExp > 255)
           raExp = (raExp % 256) + Math.floor(raExp / 256)
-        rtg.push(
-          raExp /* add alpha exponent */,
-          ctg[c + 1] + ptg[p + 1] /* add x exponent */
-        )
+        rtg.push(raExp, ctg[c + 1] + ptg[p + 1])
       }
-    }
+
     // combine like terms
     const stg: number[] = [] // simplified term group
     for (let i = 0; i < rtg.length; i += 2) {
@@ -120,41 +103,19 @@ export const GenGeneratorPoly = (degree: number): number[] => {
       const xExp = rtg[i + 1]
       let found = false
       // attempt to find a term of the same x degree
-      for (let j = 0; j < stg.length; j += 2) {
+      for (let j = 0; j < stg.length; j += 2)
         if (stg[j + 1] == xExp) {
           // recall addition in GF(256) is just XOR
           // TODO swap with alpha value, and xor, then swap back to alpha
           // swap the alpha exponent with the alpha value
-          const aExpVal = GF256AlphaToInt[stg[j]]
           // swap the alpha exponent of what we are adding with the alpha value
-          const aExpVal2 = GF256AlphaToInt[alphaExp]
           // xor the alpha values (addition under GF(256))
-          const raExpVal = aExpVal ^ aExpVal2
           // swap the alpha value back to an alpha exponent
-          const raExp = GF256IntToAlpha[raExpVal]
-          stg[j] = raExp
+          stg[j] = GF256IntToAlpha[GF256AlphaToInt[stg[j]] ^ GF256AlphaToInt[alphaExp]]
           found = true
         }
-      }
-      if (!found) { // if the current degree wasnt found, add this term to the array
+      if (!found) // if the current degree wasnt found, add this term to the array
         stg.push(alphaExp, xExp)
-      }
-    }
-
-    // perform a final pass on the simplified term group
-    // to make sure there are no exponent terms that are greater than 255
-    for (let i = 0; i < stg.length; i += 2) {
-      if (stg[i] > 255) { // check alpha exponent
-        const alphaExp = stg[i]
-        const nExp = (alphaExp % 256) + Math.floor(alphaExp / 256)
-        stg[i] = nExp
-      }
-
-      if (stg[i + 1] > 255) { // check x exponent
-        const xExp = stg[i + 1]
-        const nExp = (xExp % 256) + Math.floor(xExp / 256)
-        stg[i + 1] = nExp
-      }
     }
 
     return stg // pass it on up to the next term group
@@ -164,6 +125,6 @@ export const GenGeneratorPoly = (degree: number): number[] => {
 export const GPToString = (terms: number[]): string => {
   let str = ''
   for (let i = 0; i < terms.length; i += 2)
-    str += `α^${terms[i]}x^${terms[i + 1]} + `
+    str += `${terms[i] == 0 ? '' : terms[i] == 1 ? 'α' : `α^${terms[i]}`}${terms[i + 1] == 0 ? '' : terms[i + 1] == 1 ? 'x' : `x^${terms[i + 1]}`} + `
   return str.slice(0, -3)
 }
